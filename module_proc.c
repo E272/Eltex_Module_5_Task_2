74950577bdea68637c7b6bfa3d683466f16f742e
Filippov Egor <egfil1998@mail.ru>
2023-05-10 12:42:23 +0700

    Correction module_proc.c

--- module_proc.c
+++ module_proc.c
@@ -5,43 +5,43 @@
 #include <linux/uaccess.h>
 #include <linux/slab.h>
  
-int len,temp;
+int len, temp;
  
 char *msg;
  
-ssize_t read_proc(struct file *filp,char *buf,size_t count,loff_t *offp )
+ssize_t read_proc(struct file *filp, char *buf, size_t count, loff_t *offp )
 {
-    if(count>temp)
+    if(count > temp)
     {
-        count=temp;
+        count = temp;
     }
-    temp=temp-count;
-    copy_to_user(buf,msg, count);
-    if(count==0)
-        temp=len;
+    temp = temp - count;
+    copy_to_user(buf, msg, count);
+    if(count == 0)
+        temp = len;
  
     return count;
 }
  
-ssize_t write_proc(struct file *filp,const char *buf,size_t count,loff_t *offp)
+ssize_t write_proc(struct file *filp, const char *buf, size_t count, loff_t *offp)
 {
-    copy_from_user(msg,buf,count);
-    len=count;
-    temp=len;
+    copy_from_user(msg, buf, count);
+    len = count;
+    temp = len;
     return count;
 }
  
-struct file_operations proc_fops = {
-read:
+struct proc_ops proc_fops = {
+proc_read:
     read_proc,
-write:
+proc_write:
     write_proc
 };
  
 void create_new_proc_entry(void)  //use of void for no arguments is compulsory now
 {
-    proc_create("hello",0,NULL, &proc_fops);
-    msg=kmalloc(10*sizeof(char), GFP_KERNEL);
+    proc_create("hello", 0, NULL, &proc_fops);
+    msg = kmalloc(10 * sizeof(char), GFP_KERNEL);
 }
  
  
